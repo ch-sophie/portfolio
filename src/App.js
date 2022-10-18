e481abca7fac527e0511b3365bacce2e2fabc9e8
Sophie Cheng <chengsophie25@gmail.com>
2022-10-18 12:39:53 +0200

    commit portfolio

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import {Route, Routes} from "react-router-dom";
 import Main from "./components/main";
 import About from "./components/about";
+import Portfolio from "./components/portfolio";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
     <Routes>
       <Route exact path="/" element={<Main />} />
       <Route path="/about" element={<About />} />
+      <Route path="/portfolio" element={<Portfolio />} />
     </Routes>
     </div>
   );
---
+++ src/components/portfolio.js
@@ -0,0 +1,73 @@
+import "../App.css";
+import { Link } from "react-router-dom";
+
+function Portfolio() {
+    return(
+        <div className="flex-container-portfolio">
+            <nav className="navPort">
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/portfolio">Portfolio</Link>
+            </nav>
+            <div className="cardblock">
+                {/* card 1 */}
+                <section className="cardPort">
+                    <div className="flip-card-portfolio">
+                        <div className="flip-card-inner-portfolio">
+                            <div className="flip-card-front-portfolio">
+                                <img className="cardimage" src="" alt="Project" style={{width:"350px", height:"260px"}} />
+                            </div>
+                            <div className="flip-card-back-portfolio">
+                                <h1 className="titleProject">Cookie Clicker project</h1>
+                                <h3 className="subProject">In HTML/CSS/Javascript</h3>
+                                <p className="pProject"><a className="linkcard" href="https://github.com/ch-sophie/Cookie-clicker"> See the project here</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                {/* card 2 */}
+                <section className="cardPort">
+                    <div className="flip-card-portfolio">
+                        <div className="flip-card-inner-portfolio">
+                            <div className="flip-card-front-portfolio">
+                                <img className="cardimage" src="" alt="Project" style={{width:"350px", height:"260px"}} />
+                            </div>
+                            <div className="flip-card-back-portfolio">
+                                <h1 className="titleProject">Food guide project</h1>
+                                <h3 className="subProject">In React, Node JS and MongoDB</h3>
+                                <p className="pProject"><a className="linkcard" href="https://github.com/ch-sophie/food-guide"> See the project here</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                {/* card 3 */}
+                <section className="cardPort">
+                    <div className="flip-card-portfolio">
+                        <div className="flip-card-inner-portfolio">
+                            <div className="flip-card-front-portfolio">
+                                <img className="cardimage" src="" alt="Project" style={{width:"350px", height:"260px"}} />
+                            </div>
+                            <div className="flip-card-back-portfolio">
+                                <h1 className="titleProject">Random quotes generator</h1>
+                                <h3 className="subProject">In React JS</h3>
+                                <p className="pProject"><a className="linkcard" href="https://github.com/ch-sophie/random-quotes-generator"> See the project here</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+            <section className="iconsPort">
+            <a target="blank" href="https://www.linkedin.com/in/sophie-cheng03/"><i class="fa-brands fa-linkedin"></i></a>
+            <a target="blank" href="https://github.com/ch-sophie"><i class="fa-brands fa-github"></i></a>
+            </section>
+            <div class="twoPort">
+                <section className="footerPort">
+                    <p><q>Continuous effort, not strength or intelligence, is the key to unlocking our potential.</q></p>
+                    <i class="fa-regular fa-copyright"></i> 2022
+                </section>
+            </div>
+        </div>
+    )
+}
+
+export default Portfolio;
